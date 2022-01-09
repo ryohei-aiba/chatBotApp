35d08b805db55b47c7e375bfcee13b5e7fa8f6e1
Ryohei Aiba <ibaaaan@icloud.com>
2022-01-09 17:46:06 +0900

    modify Chats Chat

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,23 @@ export default class App extends React.Component {
     })
   }
 
+  initChats = ()=> {
+    const initDataset = this.state.dataset[this.state.currentId];
+    const chat = {
+      text: initDataset.question,
+      type: "question"
+    }
+
+    const chats = this.state.chats
+    chats.push(chat)
+
+    this.setState({
+      chats: chats
+    })
+  }
+
   componentDidMount() {
+    this.initChats()
     this.initAnswer()
   }
   
@@ -32,7 +48,7 @@ export default class App extends React.Component {
     return (
       <section className="c-section">
         <div className="c-box">
-          <Chats />
+          <Chats chats={this.state.chats}/>
           <AnswersList answers={this.state.answers}/>
         </div>
       </section>
--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,25 @@
 import React from "react";
+import ListItem from '@mui/material/ListItem';
+import ListItemText from '@mui/material/ListItemText';
+import ListItemAvatar from '@mui/material/ListItemAvatar';
+import Avatar from '@mui/material/Avatar';
+import Typography from '@mui/material/Typography';
+import { getThemeProps } from "@mui/system";
 
-const Chat = ()=> {
 
+const Chat = (props)=> {
+  const isQuestion = (props.type === "question");
+  const classes = isQuestion ? 'p-chat__row' : 'p-chat__reverse' 
+  return(
+    <ListItem>
+        <ListItemAvatar>
+          <Avatar alt="icon" src="/static/images/avatar/1.jpg" />
+        </ListItemAvatar>
+        <div className="p-chat__bubble">
+          {props.text}
+        </div>
+    </ListItem>
+  )
 }
 
 export default Chat;
