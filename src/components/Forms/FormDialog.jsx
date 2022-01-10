153e36eb98b808e8d4a371c8f48776107507dd8c
Ryohei Aiba <ibaaaan@icloud.com>
2022-01-11 05:49:52 +0900

    Modify form input

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -6,38 +6,56 @@ import DialogActions from '@mui/material/DialogActions';
 import DialogContent from '@mui/material/DialogContent';
 import DialogContentText from '@mui/material/DialogContentText';
 import DialogTitle from '@mui/material/DialogTitle';
+import TextInput from './TextInput';
 
 
 export default class FormDialog extends React.Component{
   constructor(props){
     super(props);
     this.state = {
-      open: false
+      name: "",
+      email: "",
+      description: ""
     }
+
+    this.inputName = this.inputName.bind(this)
+    this.inputEmail = this.inputEmail.bind(this)
+    this.inputDescription = this.inputDescription.bind(this)
+  }
+
+  inputName = (e)=> {
+    this.setState({name: e.target.value})
+  }
+
+  inputEmail = (e)=> {
+    this.setState({email: e.target.value})
+  }
+
+  inputDescription = (e)=> {
+    this.setState({description: e.target.value})
   }
   
   render() {
     return(
       <Dialog open={this.props.open} onClose={this.handleClose}>
-        <DialogTitle>Subscribe</DialogTitle>
+        <DialogTitle>お問い合わせフォーム</DialogTitle>
         <DialogContent>
-          <DialogContentText>
-            To subscribe to this website, please enter your email address here. We
-            will send updates occasionally.
-          </DialogContentText>
-          <TextField
-            autoFocus
-            margin="dense"
-            id="name"
-            label="Email Address"
-            type="email"
-            fullWidth
-            variant="standard"
-          />
+            <TextInput
+              label={"お名前(必須)"} multiline={false} rows={1}
+              value ={this.state.name} type={"text"} onChange={this.inputName}
+            />
+            <TextInput
+              label={"メールアドレス(必須)"} multiline={false} rows={1}
+              value ={this.state.email} type={"email"} onChange={this.inputEmail}
+            />
+            <TextInput
+              label={"お問い合わせ内容(必須)"} multiline={true} rows={5}
+              value ={this.state.description} type={"text"} onChange={this.inputDescription}
+            />
         </DialogContent>
         <DialogActions>
-          <Button onClick={this.props.handleClose}>Cancel</Button>
-          <Button onClick={this.props.handleClose}>Subscribe</Button>
+          <Button variant="outlined" onClick={this.props.handleClose}>キャンセル</Button>
+          <Button variant="outlined" onClick={this.props.handleClose}>送信</Button>
         </DialogActions>
       </Dialog>
     )
