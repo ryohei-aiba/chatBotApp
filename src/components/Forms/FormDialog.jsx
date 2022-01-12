8dee75e6563687aaf78aa55e2a6906635422401d
Ryohei Aiba <ibaaaan@icloud.com>
2022-01-12 21:24:06 +0900

    Modify FormDialog

--- src/components/Forms/FormDialog.jsx
+++ src/components/Forms/FormDialog.jsx
@@ -40,7 +40,27 @@ export default class FormDialog extends React.Component{
     const email = this.state.email
     const description = this.state.description
 
-    
+    const payload = {
+      text: "お問い合わせがありました。\n" +
+            "お名前: " + name + "\n" +
+            "Email: " + email + "\n" +
+            "お問い合わせ内容:\n" + description
+    }
+
+    const url = "incoming webhookのURL"
+
+    fetch(url, {
+      method: 'POST',
+      body: JSON.stringify(payload)
+    }).then(()=> {
+      alert("送信が完了しました。")
+      this.setState({
+        name: "",
+        email: "",
+        description: ""
+      })
+      return this.props.handleClose()
+    })
   }
   
   render() {
@@ -63,7 +83,7 @@ export default class FormDialog extends React.Component{
         </DialogContent>
         <DialogActions>
           <Button variant="outlined" onClick={this.props.handleClose}>キャンセル</Button>
-          <Button variant="outlined" onClick={this.props.submitForm}>送信</Button>
+          <Button variant="outlined" onClick={this.submitForm}>送信</Button>
         </DialogActions>
       </Dialog>
     )
